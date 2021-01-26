8dff6b0edbe3fa87d93e5ea14741ceaaa6f5b4cd
aspeedi777 <aspeedi777@gmail.com>
2021-01-27 00:25:55 +0100

    first commit

---
+++ src/main/java/com/przytula/passwort_wallet/PasswortWalletApplication.java
@@ -0,0 +1,13 @@
+package com.przytula.passwort_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswortWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswortWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/przytula/passwort_wallet/PasswortWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przytula.passwort_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswortWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
