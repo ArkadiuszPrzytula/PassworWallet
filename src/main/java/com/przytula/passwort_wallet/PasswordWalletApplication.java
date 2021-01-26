a4dfec18539bb34743a67315cfe99e9e5b14500e
aspeedi777 <aspeedi777@gmail.com>
2021-01-27 00:38:56 +0100

    configure database

--- src/main/java/com/przytula/passwort_wallet/PasswordWalletApplication.java
+++ src/main/java/com/przytula/passwort_wallet/PasswordWalletApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PasswortWalletApplication {
+public class PasswordWalletApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PasswortWalletApplication.class, args);
+        SpringApplication.run(PasswordWalletApplication.class, args);
     }
 
 }
